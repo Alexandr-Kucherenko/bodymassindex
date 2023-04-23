18daf1dfe6cc60f997f656ec81ffa66119d6986b
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-04-24 00:45:43 +0400

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,11 @@
+public class BmiService {
+    public int calculate( int kg , double heightm) {
+        double result = kg / (heightm*heightm);
+        int bmi = (int) result;
+        return bmi;
+
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int kg = 98;
+        double heightm = 1.87;
+        double bmi = service.calculate( kg , heightm);
+        System.out.println(bmi);
+    }
+}
